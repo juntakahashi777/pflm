6b30b433bf26ec6272001839a847e138a1e096bd
Jun <jtakahas@princeton.edu>
2014-04-12 17:21:07 -0400

    started ios project

---
+++ Passes for Latemeal/Passes for Latemeal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Passes for Latemeal
+//
+//  Created by jun on 4/12/14.
+//  Copyright (c) 2014 Passes For Latemeal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Passes for Latemeal/Passes for LatemealTests/Passes_for_LatemealTests.m
@@ -0,0 +1,34 @@
+//
+//  Passes_for_LatemealTests.m
+//  Passes for LatemealTests
+//
+//  Created by jun on 4/12/14.
+//  Copyright (c) 2014 Passes For Latemeal. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Passes_for_LatemealTests : XCTestCase
+
+@end
+
+@implementation Passes_for_LatemealTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
